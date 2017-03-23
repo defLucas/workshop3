6755d76e01aae04e249ee4e4f60e4727b0ca0fe1
Lucas <lucas.corriepukallus@my.jcu.edu.au>
2017-03-24 00:11:29 +1000

    coverts celsius to fahrenheit and vice versa with error checking

--- Temperature.py
+++ Temperature.py
@@ -1,19 +1,33 @@
 def main():
-    MENU= "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
-            print("Result: {:.2f} F".format(fahrenheit))
+            celsius = getCelsius()
+            break
         elif choice == "F":
-            fahrenheit = float(input("Celsius: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
-            print("Result: {:.2f} F".format(celsius))
+            fahrenheit = getFahrenheit()
+            break
         else:
             print("Invalid option")
             print(MENU)
             choice = input(">>> ").upper()
-            print("Thank you.")
+    print("Thank you.")
+
+
+def getFahrenheit():
+    fahrenheit = float(input("Celsius: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print("Result: {:.2f} F".format(celsius))
+    return fahrenheit
+
+
+def getCelsius():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+    return celsius
+
+
 main()
